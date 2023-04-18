64a521ae925b312af444c179bdd510b4fc2df48f
Lily Demos <edemos@ucsc.edu>
2023-04-18 15:08:21 -0700

    new file:   assets/blip_select12.wav 	new file:   assets/explosion.png 	new file:   assets/explosion38.wav 	new file:   assets/rocket.png 	new file:   assets/rocket_shot.wav 	new file:   assets/spaceship.png 	new file:   assets/starfield.png 	modified:   index.html 	modified:   src/main.js 	new file:   src/prefabs/Rocket.js 	new file:   src/scenes/Menu.js 	new file:   src/scenes/Play.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
+
+//UI size
+let borderUIsize = game.config.height / 15;
+let borderPadding = borderUIsize / 3;
+
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
---
+++ src/scenes/Play.js
@@ -0,0 +1,38 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload() {
+        //load images and tilesprite
+        this.load.image("rocket", "./assets/rocket.png");
+        this.load.image("spaceship", "./assets/spaceship.png");
+        this.load.image("starfield", "./assets/starfield.png");
+    }
+
+    create() {
+        //place tilesprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, "starfield").setOrigin(0, 0);
+
+        //green UI background
+        this.add.rectangle(0, borderUIsize + borderPadding, game.config.width, borderUIsize * 2, 0x00FF00).setOrigin(0, 0);
+        //white borders
+        this.add.rectangle(0, 0, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, game.config.height - borderUIsize, game.config.width, borderUIsize, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(0, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(game.config.width - borderUIsize, 0, borderUIsize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+
+        //add rocket(p1)
+        this.p1Rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUIsize - borderPadding, "rocket").setOrigin(0.5, 0);
+
+        //define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
+    }
+}
