f9d8b1d3a101e7ba42f148804ed0ca5c5c106e82
Lily Demos <edemos@ucsc.edu>
2023-04-18 17:31:22 -0700

    modified:   src/prefabs/Rocket.js 	modified:   src/scenes/Play.js

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,5 +5,33 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         //add object to existing scene
         scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
+    }
+
+    update() {
+        //left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUIsize + this.width) {
+                this.x -= this.moveSpeed;
+            }
+            else if(keyRIGHT.isDown && this.x <= game.config.width - borderUIsize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+
+        //firing
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+        //if fired move up
+        if(this.isFiring && this.y >= borderUIsize * 3 + borderPadding){
+            this.y -= this.moveSpeed;
+        }
+        //reset on miss
+        if(this.y <= borderUIsize * 3 + borderPadding) {
+            this.isFiring = false;
+            this.y = game.config.height - borderUIsize - borderPadding;
+        }
     }
 }
