c660d2a7806889fbb1eec54e305201a46cfb08a1
Lily Demos <edemos@ucsc.edu>
2023-04-21 15:36:59 -0700

    new file:   defs/phaser.d.ts 	new file:   jsconfig.json 	modified:   src/prefabs/Rocket.js 	modified:   src/prefabs/Spaceship.js 	modified:   src/scenes/Menu.js 	modified:   src/scenes/Play.js

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -7,6 +7,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.isFiring = false;
         this.moveSpeed = 2;
+        this.sfxRocket = scene.sound.add("sfx_rocket");
     }
 
     update() {
@@ -21,8 +22,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         //firing
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();
         }
         //if fired move up
         if(this.isFiring && this.y >= borderUIsize * 3 + borderPadding){
